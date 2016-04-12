33a964bb0c72f0b7123bddb33179b66d78a5a5c3
sialvsic <sialvsic@outlook.com>
2016-04-12 23:29:32 +0800

    Extract a index components

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -4,12 +4,30 @@ var ReactDOM = require('react-dom');
 var Navigation = require('./components/navigation/navigation.component');
 var Scroll = require('./components/scroll/scroll.component');
 var Slide = require('./components/slide/slide.component');
+var Account = require('./components/account/account.component');
+
+
+var Index = React.createClass({
+  componentWillMount: ()=> {
+  },
+
+
+  render: ()=> {
+    return (
+        <div >
+          <Navigation>
+            <Account/>
+          </Navigation>
+          <Scroll/>
+          <Slide/>
+        </div>
+    )
+  }
+});
+
 
 ReactDOM.render(
-    <div>
-      <Navigation/>
-      <Scroll/>
-      <Slide/>
-    </div>,
+    <Index/>
+    ,
     document.getElementById('index-page')
 );
