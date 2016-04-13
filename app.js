fa3093abf1d450b173b4cc86fb741158e13f8bc0
sialvsic <sialvsic@outlook.com>
2016-04-13 23:38:46 +0800

    set session and install some modules

--- app.js
+++ app.js
@@ -3,8 +3,27 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var yamlConfig = require('node-yaml-config');
+var mongoConn = require('./services/mongo-conn');
+var MongoStore = require('connect-mongo')(session);
 
+var config = yamlConfig.load(__dirname + '/config/config.yml');
 var route = require('./routes/route');
+
+
+var env = ['production', 'test', 'staging'].indexOf(process.env.NODE_ENV) < 0 ? 'development' : process.env.NODE_ENV;
+
+console.log(config.sessionTtl);
+app.use(cookieParser());
+app.use(session({
+  secret: 'Time-machine', resave: false, saveUninitialized: false,
+  store: new MongoStore({
+    url: config.database,
+    ttl: config.sessionTtl
+  })
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -13,9 +32,11 @@ app.use(express.static(__dirname + '/public'));
 
 route.setRoutes(app);
 
-var server = app.listen(5299, function () {
+var server = app.listen(config.port, function () {
 
   var port = server.address().port;
-
+  console.log('Current environment is: ' + env);
   console.log('App listening at http://localhost:' + port);
+  mongoConn.start(config.database);
+
 });
