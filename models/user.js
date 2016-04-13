442eec1c71030588f75c96d04cadc5c95da3a5f4
sialvsic <sialvsic@outlook.com>
2016-04-13 23:37:34 +0800

    set user schema

---
+++ models/user.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var constant = require('../mixin/constant');
+var yamlConfig = require('node-yaml-config');
+var config = yamlConfig.load('./config/config.yml');
+
+var _timeBase = 90;
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  userId: Number,
+  email: String,
+  mobilePhone: String,
+  password: String,
+  createTime: Number
+
+});
+
+
+module.exports = mongoose.model('users', userSchema);
