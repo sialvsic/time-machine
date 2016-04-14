268c79067f5eccc699b1307c4c8fd173814cc935
sialvsic <sialvsic@outlook.com>
2016-04-14 10:52:01 +0800

    modified password use md5

--- tools/seeds/refresh.js
+++ tools/seeds/refresh.js
@@ -22,22 +22,22 @@ db.once('open', () => {
           {
             email: '11111111@qq.com',
             mobilePhone: '13111111111',
-            password: '12345678',
+            password: '25d55ad283aa400af464c76d713c07ad',
             creatData: '1451117203'
           }, {
             email: '22222222@qq.com',
             mobilePhone: '13122222222',
-            password: '12345678',
+            password: '25d55ad283aa400af464c76d713c07ad',
             creatData: '1451117223'
           }, {
             email: '33333333@qq.com',
             mobilePhone: '13133333333',
-            password: '12345678',
+            password: '25d55ad283aa400af464c76d713c07ad',
             creatData: '1451117223'
           }, {
             email: '44444444@qq.com',
             mobilePhone: '13144444444',
-            password: '12345678',
+            password: '25d55ad283aa400af464c76d713c07ad',
             creatData: '1451117223'
           }
         ], function () {
