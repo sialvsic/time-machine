53852398e870b402e390ea4bae2df65e16557bb0
sialvsic <sialvsic@outlook.com>
2016-04-17 15:48:12 +0800

    use multer to handle file upload

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 'use strict';
 
 var express = require('express');
-var app = express();
+var multer = require('multer');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var yamlConfig = require('node-yaml-config');
 var mongoConn = require('./services/mongo-conn');
 var MongoStore = require('connect-mongo')(session);
+var app = express();
+
 
 var config = yamlConfig.load(__dirname + '/config/config.yml');
 var route = require('./routes/route');
@@ -15,7 +17,6 @@ var route = require('./routes/route');
 
 var env = ['production', 'test', 'staging'].indexOf(process.env.NODE_ENV) < 0 ? 'development' : process.env.NODE_ENV;
 
-console.log(config.sessionTtl);
 app.use(cookieParser());
 app.use(session({
   secret: 'Time-machine', resave: false, saveUninitialized: false,
@@ -27,6 +28,26 @@ app.use(session({
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(multer({
+  dest: './public/vedio',
+  filefilet: function () {
+
+  },
+  onFileUploadStart: function (file) {
+    console.log("upload start");
+  },
+
+  onFileUploadComplete: function (file) {
+    console.log("upload complete");
+  },
+  rename: function (fieldname, filename) {
+
+    console.log(fieldname);
+    console.log(filename);
+    return filename;
+  }
+}));
 
 app.use(express.static(__dirname + '/public'));
 
