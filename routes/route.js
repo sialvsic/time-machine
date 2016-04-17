37d11a17130fff39581e95abb19e377049a25b96
sialvsic <sialvsic@outlook.com>
2016-04-17 15:49:55 +0800

    add sub route

--- routes/route.js
+++ routes/route.js
@@ -4,5 +4,7 @@ exports.setRoutes = function (app) {
   //app.use('/', require('./routers/index'));
   app.use('/register', require('./routers/register'));
   app.use('/login', require('./routers/login'));
+  app.use('/account', require('./routers/account'));
+  app.use('/upload', require('./routers/upload'));
 
 };
