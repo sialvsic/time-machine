6ebe64a4818f00de3d6c8ace6273655e5fd91ec1
sialvsic <sialvsic@outlook.com>
2016-04-20 00:49:33 +0800

    add a vedio model  schema

---
+++ models/vedio.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var constant = require('../mixin/constant');
+var yamlConfig = require('node-yaml-config');
+var config = yamlConfig.load('./config/config.yml');
+
+var Schema = mongoose.Schema;
+
+var vedioSchema = new Schema({
+  userId: String,
+  originalname: String,   //原始的视频文件名
+  encoding: String,       //编码
+  mimetype: String,       //多用途互联网邮件扩展类型
+  path: String,           //存储视频的路径
+  extension: String,      //扩展名
+  size: String,           //文件大小
+  truncated: {type: Boolean, default: false},       //是否进行压缩
+  buffer: {type: String, default: false},           //是否是流媒体
+  title: String,          //视频标题
+  category: String,       //视频分类
+  label: String,          //视频标签
+  description: String,    //视频描述
+  isDetailsInfoComplete: {type: Boolean, default: false},       //是否信息完整
+  createTime: Number      //视频上传的时间
+
+});
+
+
+module.exports = mongoose.model('vedios', vedioSchema);
