4b9194164e09d20819b95da938568cae383c4bfb
sialvsic <sialvsic@outlook.com>
2016-04-25 00:20:06 +0800

    add movie component to the index page

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -5,6 +5,7 @@ var Navigation = require('./components/navigation/navigation.component');
 var Scroll = require('./components/scroll/scroll.component');
 var Slide = require('./components/slide/slide.component');
 var Account = require('./components/account/account.component');
+var Movie = require('./components/movie/movie.component.js');
 
 
 var Index = React.createClass({
@@ -20,6 +21,8 @@ var Index = React.createClass({
           </Navigation>
           <Scroll/>
           <Slide/>
+
+          <Movie/>
         </div>
     )
   }
