be9bb1e1d7712cdbde04667502f93a6f0b52be56
sialvsic <sialvsic@outlook.com>
2016-04-27 11:25:56 +0800

    add session-check

--- mixin/get-jump-control.js
+++ mixin/get-jump-control.js
@@ -1,64 +1,19 @@
 'use strict';
 
 function jumpControl(data) {
+
   var isLoged = data.isLoged;
-  var isPaperCommited = data.isPaperCommited;
-  var isDetailed = data.isDetailed;
-  var isAgreed = data.isAgreed;
-  var isThirdParty = data.isThirdParty;
+
+  //设计思路:
+  //当满足 condition的情况下 发向 originPath的请求会跳转到 targetPath
 
   return [{
     originPath: [
-      'homework.html',
-      'logic-puzzle.html',
-      'progress.html',
-      'start.html',
-      'dashboard.html'
+      'vedio.html',
+      'upload.html'
     ],
     targetPath: '/register.html',
     condition: !isLoged
-  }, {
-    originPath: [
-      'logic-puzzle.html',
-      'start.html'
-    ],
-    targetPath: '/dashboard.html',
-    condition: isLoged && isPaperCommited && isDetailed
-  }, {
-    originPath: [
-      'homework.html',
-      'logic-puzzle.html',
-      'progress.html',
-      'start.html',
-      'dashboard.html'
-    ],
-    targetPath: '/user-center.html',
-    condition: isLoged && !isDetailed
-  }, {
-    originPath: [
-      'homework.html'
-    ],
-    targetPath: 'dashboard.html',
-    condition: !isPaperCommited
-  }, {
-    originPath: [
-      'start.html'
-    ],
-    targetPath: 'dashboard.html',
-    condition: isAgreed
-  }, {
-    originPath: [
-      'user-center.html'
-    ],
-    targetPath: 'register.html',
-    condition: !(isLoged || isThirdParty)
-  }, {
-    originPath: [
-      'register.html'
-    ],
-    targetPath: 'dashboard.html',
-    condition: isLoged
-  }
-  ];
+  }];
 }
 module.exports = jumpControl;
