d5e27004b12f6788d90218a3b5524d272669940e
sialvsic <sialvsic@outlook.com>
2016-05-16 02:38:46 +0800

    modified shareQQ style

--- source/scripts/components/video-func/video-func.component.js
+++ source/scripts/components/video-func/video-func.component.js
@@ -33,6 +33,30 @@ var VideoFunc = React.createClass({
     VideoFuncActions.getLoginStatus();
   },
 
+  componentDidMount: function () {
+    var href = window.location.href;
+    var p = {
+    url:'http://www.jiathis.com', /*获取URL，可加上来自分享到QQ标识，方便统计*/
+    desc:'快来看看吧! ' + href, /*分享理由(风格应模拟用户对话),支持多分享语随机展现（使用|分隔）*/
+    title:'时光机', /*分享标题(可选)*/
+    summary:'这是来自时光机的分享', /*分享摘要(可选)*/
+    pics:' ', /*分享图片(可选)*/
+    flash: '', /*视频地址(可选)*/
+    site:'', /*分享来源(可选) 如：QQ分享*/
+    style:'103',
+    width:50,
+    height:16
+    };
+    var s = [];
+    for(var i in p){
+    s.push(i + '=' + encodeURIComponent(p[i]||''));
+    }
+    jQuery('#li-list').append(['<a class="qcShareQQDiv" href="http://connect.qq.com/widget/shareqq/index.html?',s.join('&'),'" target="_blank">分享到QQ</a>'].join(''));
+    // document.write(['<a class="qcShareQQDiv" href="http://connect.qq.com/widget/shareqq/index.html?',s.join('&'),'" target="_blank">分享到QQ</a>'].join(''));
+
+  },
+
+
   thumb: function () {
     if(!this.state.isLogin){
       page('register.html');
@@ -115,7 +139,7 @@ var VideoFunc = React.createClass({
         <div id="videofunc-div">
           <div className="container">
             <section className="content">
-              <ol className="">
+              <ol id="li-list">
                 <li className="" onClick={this.download}>
                   <span>
                     <i className="fa fa-download" aria-hidden="true"/>
