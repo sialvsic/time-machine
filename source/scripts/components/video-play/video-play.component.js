7472670f28a3bac63271633b08fac3b6a401c179
sialvsic <sialvsic@outlook.com>
2016-05-17 14:01:10 +0800

    modified a video plugin to fix play flv video

--- source/scripts/components/video-play/video-play.component.js
+++ source/scripts/components/video-play/video-play.component.js
@@ -11,25 +11,68 @@ function isEmptyObject(Object) {
 
 var VideoPlay = React.createClass({
 
+  componentDidUpdate:function(){
+    console.log('i am in ');
+    var container = document.getElementById("video-play");
+
+     if(!isEmptyObject(this.props.videoPlayInfo)){
+       var src = this.props.videoPlayInfo.path.replace('public/','');
+       var type = this.props.videoPlayInfo.mimetype;
+
+       flowplayer(container, {
+          autoplay: true,
+           clip: {
+               sources: [
+                     { type: type,
+                       src:  src }
+               ]
+           }
+       });
+     }
+    // install flowplayer into selected container
+
+  },
+
   render: function () {
 
-    var source = isEmptyObject(this.props.videoPlayInfo) ? '' : (
-        <source src={this.props.videoPlayInfo.path.replace('public/','')}
-                type={this.props.videoPlayInfo.mimetype}/>
-    );
 
 
     return (
-        <div id="videoplay-div">
-          <div>
-            <video id="my-video" className="video-js vjs-big-play-centered" controls
-                   data-setup='{ "playbackRates": [1, 1.5, 2] ,"autoplay": true, "preload": "auto"}'>
-              {source}
-            </video>
-          </div>
-        </div>
+      <div id="videoplay-div">
+         <div id="video-play">
+         </div>
+
+      </div>
+
     )
   }
 });
 
 module.exports = VideoPlay;
+
+// var source = isEmptyObject(this.props.videoPlayInfo) ? '' : (
+//
+//     <source src={this.props.videoPlayInfo.path.replace('public/','')}
+//           type={this.props.videoPlayInfo.mimetype}/>
+//
+//
+// );
+// <div className="flowplayer" data-ratio="0.6" >
+//   <video>
+//     {source}
+//   </video>
+// </div>
+
+// var source = isEmptyObject(this.props.videoPlayInfo) ? '' : (
+//     <source src={this.props.videoPlayInfo.path.replace('public/','')}
+//             type={this.props.videoPlayInfo.mimetype}/>
+// );
+//
+// <div id="videoplay-div">
+//   <div>
+//     <video id="my-video" className="video-js vjs-big-play-centered" controls
+//            data-setup='{ "playbackRates": [1, 1.5, 2] ,"autoplay": true, "preload": "auto","techOrder": ["html5", "flash"]}'>
+//       {source}
+//     </video>
+//   </div>
+// </div>
